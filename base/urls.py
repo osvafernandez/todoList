92165bb929873df83c94abaf548ae071a2913802
osvafernandez <osvanaranjo96@gmail.com>
2023-06-06 10:48:01 -0400

    login and task added

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', views.CusomLoginView.as_view(), name='login'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.TaskList.as_view(), name="tasks"),
     path('task/<int:pk>', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
